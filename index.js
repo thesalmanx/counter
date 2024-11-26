c006932d6c7672e2cbf23c98b9c64a0ec7e93a77
Muhammad Salman <111143401+thesalmanx@users.noreply.github.com>
2024-11-26 22:19:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function increment() {
+  document.getElementById("count-el").innerText = ++count;
+}
+
+function save() {
+  document.getElementById("message").innerText += " - " + count;
+  count = 0;
+  document.getElementById("count-el").innerText = 0;
+}
+
+function reset() {
+  document.getElementById("message").innerText = "Previous entries";
+  count = 0;
+  document.getElementById("count-el").innerText = 0;
+}
