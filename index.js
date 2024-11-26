b4beea0bde0f59e44f06b8e517c35e8b9b28fad9
Muhammad Salman <111143401+thesalmanx@users.noreply.github.com>
2024-11-26 22:37:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+
 let count = 0;
 
-function increment() {
-  document.getElementById("count-el").innerText = ++count;
+function increment(){
+    document.getElementById("count-el").innerText = ++count;
 }
 
-function save() {
-  document.getElementById("message").innerText += " - " + count;
-  count = 0;
-  document.getElementById("count-el").innerText = 0;
+function save(){
+    document.getElementById("message").innerText += " - " + count; 
+    count = 0;
+    document.getElementById("count-el").innerText = 0;
 }
 
-function reset() {
-  document.getElementById("message").innerText = "Previous entries";
-  count = 0;
-  document.getElementById("count-el").innerText = 0;
+function reset(){
+    document.getElementById("message").innerText = "Previous entries";
+    count = 0;
+    document.getElementById("count-el").innerText = 0;
+}
+
+function error(){
+    document.getElementById("errormsg").innerText = "There is an error, Please try again later"
 }
